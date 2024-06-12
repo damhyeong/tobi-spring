7f56d23c7790f0978f4a8b6238a6e883c1662f16
damhyeong <rhdwhdals8@naver.com>
2024-06-12 15:26:22 +0900

    Creating Folder Chapter1-2 because i will learn next chapter in 1.

---
+++ chapter1-2/src/test/java/springbook/Chapter12ApplicationTests.java
@@ -0,0 +1,13 @@
+package springbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
